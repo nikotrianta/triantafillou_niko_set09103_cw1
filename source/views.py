93d674016defb2bf24b2a6b14b2ef7b66260ad94
nikotrianta <43911949+nikotrianta@users.noreply.github.com>
2018-10-29 15:25:37 +0000

    Add files via upload

--- source/views.py
+++ source/views.py
@@ -5,6 +5,10 @@ from flask import render_template
 def index():
 	return render_template('index.html')
 
+@app.route('/home')
+def home():
+	return render_template('home.html')
+
 @app.route('/about')
 def about():
 	return render_template('about.html')
