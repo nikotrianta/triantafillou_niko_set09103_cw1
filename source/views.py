724e746726bc1b27f35fc862b620b7177db570e6
nikotrianta <43911949+nikotrianta@users.noreply.github.com>
2018-10-29 14:45:19 +0000

    Add files via upload

--- source/views.py
+++ source/views.py
@@ -9,6 +9,14 @@ def index():
 def about():
 	return render_template('about.html')
 
+@app.route('/experience')
+def about():
+	return render_template('experience.html')
+
+@app.route('/raiding')
+def about():
+	return render_template('raiding.html')
+
 @app.route('/dungeons')
 def dungeons():
 	return render_template('dungeons.html')
