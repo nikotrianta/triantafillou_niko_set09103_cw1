9057bfcab2f17f74ef0bbbb78e578f2e0e9cd30d
nikotrianta <43911949+nikotrianta@users.noreply.github.com>
2018-11-03 01:15:39 +0000

    adding racelists

--- source/views.py
+++ source/views.py
@@ -81,6 +81,54 @@ def horde():
 def races():
 	return render_template('races.html')
 
+@app.route('/humans')
+def humans():
+	return render_template('raceslist/alliancelist/humans.html')
+
+@app.route('/dwarves')
+def dwarves():
+	return render_template('raceslist/alliancelist/dwarves.html')
+
+@app.route('/gnomes')
+def gnomes():
+	return render_template('raceslist/alliancelist/gnomes.html')
+
+@app.route('/draenei')
+def draenei():
+	return render_template('raceslist/alliancelist/draenei.html')
+
+@app.route('/nightelves')
+def nightelves():
+	return render_template('raceslist/alliancelist/nightelves.html')
+
+@app.route('/worgens')
+def worgens():
+	return render_template('raceslist/alliancelist/worgens.html')
+
+@app.route('/orcs')
+def orcs():
+	return render_template('raceslist/hordelist/orcs.html')
+
+@app.route('/forsaken')
+def forsaken():
+	return render_template('raceslist/hordelist/forsaken.html')
+
+@app.route('/tauren')
+def tauren():
+	return render_template('raceslist/hordelist/tauren.html')
+
+@app.route('/trolls')
+def trolls():
+	return render_template('raceslist/hordelist/trolls.html')
+
+@app.route('/bloodelves')
+def bloodelves():
+	return render_template('raceslist/hordelist/bloodelves.html')
+
+@app.route('/goblins')
+def goblins():
+	return render_template('raceslist/hordelist/goblins.html')
+
 @app.route('/classes')
 def characters():
 	return render_template('classes.html')
